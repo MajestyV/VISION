52b66f3291d0b03425e6f7ae1d034a13e48e90fb
MajestyV <sl4417@columbia.edu>
2024-07-22 19:56:41 +0800

    'Lingjiang_20240722'

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 # Source code files of the package: VISION
 
-from . import ElectronicEngineering as EE  # Package handling data in the field of electronic engineering (EE)
+from . import Electrica as EE  # Package handling data in the field of electronic engineering (EE)
