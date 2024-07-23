072a76930a6debd7489e1ee2c55896ea46c98a0c
MajestyV <sl4417@columbia.edu>
2024-07-23 11:57:44 +0800

    'Lingjiang_20240723'

--- src/Electrica/Analysis/FrequencyAnalysis.py
+++ src/Electrica/Analysis/FrequencyAnalysis.py
@@ -6,45 +6,70 @@ from matplotlib.pylab import mpl
 mpl.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
 mpl.rcParams['axes.unicode_minus'] = False  # 显示负号
 
-# 采样点选择1400个，由于设置的信号频率份量最高为600赫兹，根据采样定理知采样频率要大于信号频率2倍，因此这里设置采样频率为1400赫兹（即一秒内有1400个采样点，同样意思的）
-x = np.linspace(0, 1, 1400)
+class FreqAnal:
+    '''
+    This class of function is designed for analysis the frequency spectrum of time series.
+    '''
+    def __init__(self, signal_train: np.ndarray, num_freq: int):
+        self.y = signal_train
+        self.fft_y = fft(y)  # 快速傅里叶变换
+        self.n_freq = num_freq  # 衡量的频率个数
 
-# 设置须要采样的信号，频率份量有200，400和600
-y = 7 * np.sin(2 * np.pi * 200 * x) + 5 * np.sin(2 * np.pi * 400 * x) + 3 * np.sin(2 * np.pi * 600 * x)
+    def Freq(self): return self.fft_y  # 频谱图
 
-fft_y = fft(y)  # 快速傅里叶变换
+    def Freq_absolute(self): return np.abs(self.fft_y)  # 取复数的绝对值，即复数的模(双边频谱)
 
-N = 1400
-x = np.arange(N)  # 频率个数
-half_x = x[range(int(N / 2))]  # 取一半区间
+    def Freq_normalized(self): return np.abs(self.fft_y)/self.n_freq  # 归一化处理（双边频谱）
 
-abs_y = np.abs(fft_y)  # 取复数的绝对值，即复数的模(双边频谱)
-angle_y = np.angle(fft_y)  # 取复数的角度
-normalization_y = abs_y / N  # 归一化处理（双边频谱）
-normalization_half_y = normalization_y[range(int(N / 2))]  # 因为对称性，只取一半区间（单边频谱）
+    def Freq_half(self):  # 因为对称性，只取一半区间（单边频谱）
+        fft_y_normalized = np.abs(self.fft_y)/self.n_freq
+        return fft_y_normalized[range(int(self.n_freq/2))]
 
-plt.subplot(231)
-plt.plot(x, y)
-plt.title('原始波形')
+    def Phase(self): return np.angle(fft_y)  # 取复数的角度，即为相位
 
-plt.subplot(232)
-plt.plot(x, fft_y, 'black')
-plt.title('双边振幅谱(未求振幅绝对值)', fontsize=9, color='black')
 
-plt.subplot(233)
-plt.plot(x, abs_y, 'r')
-plt.title('双边振幅谱(未归一化)', fontsize=9, color='red')
+if __name__=='__main__':
+    # 采样点选择1400个，由于设置的信号频率份量最高为600赫兹，根据采样定理知采样频率要大于信号频率2倍，因此这里设置采样频率为1400赫兹（即一秒内有1400个采样点，同样意思的）
+    x = np.linspace(0, 1, 1400)
 
-plt.subplot(234)
-plt.plot(x, angle_y, 'violet')
-plt.title('双边相位谱(未归一化)', fontsize=9, color='violet')
+    # 设置须要采样的信号，频率份量有200，400和600
+    y = 7 * np.sin(2 * np.pi * 200 * x) + 5 * np.sin(2 * np.pi * 400 * x) + 3 * np.sin(2 * np.pi * 600 * x)
 
-plt.subplot(235)
-plt.plot(x, normalization_y, 'g')
-plt.title('双边振幅谱(归一化)', fontsize=9, color='green')
+    N = 1400
+    x = np.arange(N)  # 频率个数
+    half_x = x[range(int(N / 2))]  # 取一半区间
 
-plt.subplot(236)
-plt.plot(half_x, normalization_half_y, 'blue')
-plt.title('单边振幅谱(归一化)', fontsize=9, color='blue')
+    FA = FreqAnal(signal_train=y,num_freq=N)  # 创建一个frequency analysis对象
+    fft_y = FA.Freq()
+    abs_y = FA.Freq_absolute()
+    angle_y = FA.Phase()
+    normalization_y = FA.Freq_normalized()
+    normalization_half_y = FA.Freq_half()
 
-plt.show()
+    plt.subplot(231)
+    plt.plot(x, y)
+    plt.title('原始波形')
+
+    plt.subplot(232)
+    plt.plot(x, fft_y, 'black')
+    plt.title('双边振幅谱(未求振幅绝对值)', fontsize=9, color='black')
+
+    plt.subplot(233)
+    plt.plot(x, abs_y, 'r')
+    plt.title('双边振幅谱(未归一化)', fontsize=9, color='red')
+
+    plt.subplot(234)
+    plt.plot(x, angle_y, 'violet')
+    plt.title('双边相位谱(未归一化)', fontsize=9, color='violet')
+
+    plt.subplot(235)
+    plt.plot(x, normalization_y, 'g')
+    plt.title('双边振幅谱(归一化)', fontsize=9, color='green')
+
+    plt.subplot(236)
+    plt.plot(half_x, normalization_half_y, 'blue')
+    plt.title('单边振幅谱(归一化)', fontsize=9, color='blue')
+
+    plt.tight_layout()  # 调整页面分布
+
+    plt.show(block=True)
--- src/Electrica/GetData.py
+++ src/Electrica/GetData.py
@@ -33,23 +33,45 @@ def GetData_Siglent(data_file: str, skiprows: int=6, num_rows: int=500, sampling
 if __name__ == '__main__':
     # 主程序可用于在命令行终端快速提取数据到txt文件
 
-    default_mode = ('multiple')  # 默认的读取模式
+    default_mode = ('single')  # 默认的读取模式
 
-    # 默认的数据路径
+    data_dir_key = 'Lingjiang_RO'  # 数据文件夹路径字典的键
+
+    data_filename = 'SDS2354X_HD_Binary_C3_1_Analog_Trace.csv'  # 数据文件名
+
+    saving_dir_key = 'Lingjiang_RO'  # 数据保存路径字典的键
+
+
+    # 预设各类路径字典
+    # 数据文件夹路径字典
+    default_data_dir_dict = {
+        'MMW405': 'E:/Projects/EchoStateMachine/Data/ResNode测试/Activation/Raw data',  # MMW405
+        'Lingjiang_RO': 'D:/PhD_research/Jingfang Pei/Solution-processed IC/Data/OSC/RingOscillator/20240711_OSC_RO'  # Lingjiang RO
+    }
+
+    default_data_file = f"{default_data_dir_dict[data_dir_key]}/{data_filename}"  # 默认的数据文件的绝对地址
+    print(default_data_file)
+
+    # 数据保存路径字典
+    default_saving_dir_dict = {
+        'MMW405': 'D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/ResNode/Working_dir/Activation/Extracted data',  # MMW405
+        'Lingjiang_RO': 'D:/PhD_research/Jingfang Pei/Solution-processed IC/Data/OSC/RingOscillator/Working_dir'  # Lingjiang RO
+    }
+
     # example_data_directory = 'E:/Projects/EchoStateMachine/Data/ResNode测试/ResNode_20240510/Triangular'  # MMW502
     # example_data_directory = 'D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/ResNode/Working_dir/Temporary'  # MMW502临时数据文件夹
-    example_data_directory = 'E:/Projects/EchoStateMachine/Data/ResNode测试/Activation/Raw data'  # MMW405
+    # example_data_directory = 'E:/Projects/EchoStateMachine/Data/ResNode测试/Activation/Raw data'  # MMW405
     # example_data_directory = 'E:/Projects/EchoStateMachine/Data/ResNode测试/Temperary'
     # example_data_directory = 'D:/PhD_research/EchoStateMachine/Data/ResNode/Working_dir/Triangular'  # Lingjiang
 
-    example_data_filename = 'SDS2354X_HD_Binary_C1_2_Analog_Trace.csv'  # 示例数据文件
+    # example_data_filename = 'SDS2354X_HD_Binary_C1_2_Analog_Trace.csv'  # 示例数据文件
 
-    example_data_file = f"{example_data_directory}/{example_data_filename}"  # 示例数据文件的绝对地址
+    # example_data_file = f"{example_data_directory}/{example_data_filename}"  # 示例数据文件的绝对地址
 
     # default_saving_directory = os.path.abspath(os.path.join(os.getcwd(), '..'))+'/Default_data_folder'  # 默认的数据存储路径
     # default_saving_directory = 'D:/PhD_research/EchoStateMachine/Data/ResNode/Working_dir/data'  # Lingjiang
     # default_saving_directory = 'D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/ResNode/Working_dir/Activation'  # MMW502
-    default_saving_directory = 'D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/ResNode/Working_dir/Activation/Extracted data'  # MMW405
+    # default_saving_directory = 'D:/OneDrive/OneDrive - The Chinese University of Hong Kong/Desktop/ResNode/Working_dir/Activation/Extracted data'  # MMW405
 
     # 加载命令行解析器
     parser = argparse.ArgumentParser()
@@ -58,9 +80,9 @@ if __name__ == '__main__':
     parser.add_argument('--mode', metavar='-M', type=str, default=default_mode, help='Mode')  # 读取模式
 
     # 源数据文件参数
-    parser.add_argument('--data_file', metavar='-D', type=str, default=example_data_file, help='Source data file')  # 源数据文件绝对地址
-    parser.add_argument('--data_directory', metavar='-P', type=str, default=example_data_directory, help='Data directory')  # 源数据文件夹
-
+    parser.add_argument('--data_file', metavar='-D', type=str, default=default_data_file, help='Source data file')  # 源数据文件绝对地址
+    parser.add_argument('--data_directory', metavar='-P', type=str, default=default_data_dir_dict[data_dir_key],
+                        help='Data directory')  # 源数据文件夹
     # 数据读取参数
     parser.add_argument('--skiprows', metavar='-K', type=int, default=6, help='Skip rows')  # 跳过的行数
     parser.add_argument('--num_rows', metavar='-N', type=int, default=500, help='Number of rows read')  # 最大行数
@@ -69,7 +91,7 @@ if __name__ == '__main__':
     parser.add_argument('--delimiter', metavar='-B', type=str, default=',', help='Delimiter')  # 分隔符
 
     # 数据保存参数
-    parser.add_argument('--saving_directory', metavar='-S', type=str, default=default_saving_directory,
+    parser.add_argument('--saving_directory', metavar='-S', type=str, default=default_saving_dir_dict[saving_dir_key],
                         help='Saving directory')  # 数据文件
     parser.add_argument('--file_name', metavar='-F', type=str, default='data.csv', help='File name')  # 数据文件
 
@@ -77,9 +99,14 @@ if __name__ == '__main__':
 
     # 区分模式读取
     if args.mode == 'single':  # 单文件模式
+        # data = GetData_Siglent(data_file=args.data_file,
+                               # skiprows=args.skiprows, num_rows=args.num_rows, sampling_interval=args.sampling_interval,
+                               # usecols=args.usecols, delimiter=args.delimiter)
+        # Temporary
         data = GetData_Siglent(data_file=args.data_file,
-                               skiprows=args.skiprows, num_rows=args.num_rows, sampling_interval=args.sampling_interval,
+                               skiprows=args.skiprows, num_rows=100, sampling_interval=1,
                                usecols=args.usecols, delimiter=args.delimiter)
+
         np.savetxt(f"{args.saving_directory}/{args.file_name}", data, delimiter=',')  # 保存数据
 
     elif args.mode == 'multiple':  # 多文件模式
@@ -89,10 +116,12 @@ if __name__ == '__main__':
             # data = GetData_Siglent(data_file=f"{args.data_directory}/{file}",
                                    # skiprows=args.skiprows, num_rows=args.num_rows, sampling_interval=args.sampling_interval,
                                    # usecols=args.usecols, delimiter=args.delimiter)
+            # Temporary
             data = GetData_Siglent(data_file=f"{args.data_directory}/{file}",
                                    skiprows=args.skiprows, num_rows=2000,
                                    sampling_interval=2000,
                                    usecols=args.usecols, delimiter=args.delimiter)
+
             np.savetxt(f"{args.saving_directory}/{file}", data, delimiter=',')  # 保存数据
     else:
         print('Invalid mode! Please select a valid mode parameter: single or multiple ! ! !')
