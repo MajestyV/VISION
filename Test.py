8a4efd9846c628bf545718c1023133349505b18b
MajestyV <53797732+MajestyV@users.noreply.github.com>
2024-07-23 16:39:23 +0800

    MMW405_20230723

--- Test.py
+++ Test.py
@@ -1,7 +1,38 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import numpy.fft as fft
+import pandas as pd
+
+def get_fft_values(y_values, N, f_s):
+    '''
+    封装好的频谱分析函数
+    '''
+    f_values = np.linspace(0.0, f_s / 2.0, N // 2)
+    fft_values_ = fft.fft(y_values)
+    fft_values = 2.0 / N * np.abs(fft_values_[0:N // 2])
+    return f_values, fft_values
 
 if __name__ == '__main__':
-    red = (1, 0, 0)
-    pink = (1, 0.9, 0.9)
-    for i in range(200):
-        plt.plot(record[i], color=interpolateColor(pink, red, i / 200))
+    # 提取数据
+    data_directory = '/Users/liusongwei/Desktop/SolutionIC_Temporary/Data/RO'  # Macbook
+    data_filename = 'data_100000.csv'
+    data_file = f"{data_directory}/{data_filename}"
+
+    data_DF = pd.read_csv(data_file)
+    time = data_DF.values[:, 0]
+    y = data_DF.values[:, 1]
+
+    n_sample = len(time)  # 采样点数
+    f_sample = n_sample/max(time)  # 采样频率
+
+    print(n_sample,f_sample)
+
+    a, b = get_fft_values(y,N=n_sample,f_s=f_sample)
+
+    print(a,b)
+
+    plt.plot(a,b)
+    plt.ylim(-0.01,0.01)
+
+    plt.show(block=True)
+
--- src/Electrica/Analysis/FrequencyAnalysis.py
+++ src/Electrica/Analysis/FrequencyAnalysis.py
@@ -3,8 +3,10 @@ from scipy.fftpack import fft
 import matplotlib.pyplot as plt
 from matplotlib.pylab import mpl
 
-mpl.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
-mpl.rcParams['axes.unicode_minus'] = False  # 显示负号
+import pandas as pd  # 数据提取包
+
+# mpl.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
+# mpl.rcParams['axes.unicode_minus'] = False  # 显示负号
 
 class FreqAnal:
     '''
@@ -30,12 +32,26 @@ class FreqAnal:
 
 if __name__=='__main__':
     # 采样点选择1400个，由于设置的信号频率份量最高为600赫兹，根据采样定理知采样频率要大于信号频率2倍，因此这里设置采样频率为1400赫兹（即一秒内有1400个采样点，同样意思的）
-    x = np.linspace(0, 1, 1400)
+    # x = np.linspace(0, 1, 1400)
 
     # 设置须要采样的信号，频率份量有200，400和600
-    y = 7 * np.sin(2 * np.pi * 200 * x) + 5 * np.sin(2 * np.pi * 400 * x) + 3 * np.sin(2 * np.pi * 600 * x)
+    # y = 7 * np.sin(2 * np.pi * 200 * x) + 5 * np.sin(2 * np.pi * 400 * x) + 3 * np.sin(2 * np.pi * 600 * x)
 
-    N = 1400
+    # 提取数据
+    data_directory = '/Users/liusongwei/Desktop/SolutionIC_Temporary/Data/RO'  # Macbook
+    data_filename = 'data_1000.csv'
+    data_file = f"{data_directory}/{data_filename}"
+
+    data_DF = pd.read_csv(data_file)
+    time = data_DF.values[:, 0]
+    y = data_DF.values[:, 1]
+
+    # print(data_DF.values)
+
+
+    # exit()
+
+    N = len(y)
     x = np.arange(N)  # 频率个数
     half_x = x[range(int(N / 2))]  # 取一半区间
 
